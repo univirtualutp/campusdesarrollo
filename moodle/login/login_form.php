e2c36a703cfee0bdaa654080e0db8dfca6b683f0
arenasdaniel <daniel.pardo@utp.edu.co>
2024-09-04 15:13:10 -0500

    login

---
+++ moodle/login/login_form.php
@@ -0,0 +1,14 @@
+class login_form extends moodleform {
+    // Definición del formulario
+    public function definition() {
+        $mform = $this->_form;
+
+        $mform->addElement('text', 'username', get_string('username'));
+        $mform->setType('username', PARAM_NOTAGS);
+
+        $mform->addElement('password', 'password', get_string('password'));
+        $mform->setType('password', PARAM_RAW);
+
+        $this->add_action_buttons(false, get_string('login'));
+    }
+}
